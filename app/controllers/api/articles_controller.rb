8de5936b91a371bcd8d3742dd8faf62a438bb86a
Alexander Yordanov <yordanova@vmware.com>
2023-01-02 23:56:26 +0200

    React functionality to search by term

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,22 +1,34 @@
 module Api
 	class ArticlesController < ApplicationController
-		def index
-			articles = [{
+		ARTICLES = [{
 				Title: "test title",
 				Link: "https://www.cnn.com/travel/article/indonesia-criminal-code-tourists-intl-hnk/index.html",
 				GUID: "testguid",
 				PublishDate: "2022-12-07T10:59:15Z",
+				Feed: "test feed1",
 				Description: "International travelers have been flocking back to the popular resort island of Bali as the Covid pandemic subsides, prompting hopes that Indonesia's battered tourism industry is on the road to recovery."
 			},
 			{
 				Title: "test title",
 				Link: "https://www.cnn.com/travel/article/indonesia-criminal-code-tourists-intl-hnk/index.html",
 				GUID: "testguid",
-				PublishDate: "2022-12-07T10:59:15Z",
+				Feed: "test feed8",
+				PublishDate: "2022-12-07T10:32:15Z",
 				Description: "test desc"
 			}]
+		def index
+			puts params["search_term"]
+			articles = filter_by(params["search_term"])
+			puts articles
 
 			render(json: {articles: articles})
+		end
+
+		def filter_by(search_term)
+			if search_term.blank?
+				return ARTICLES
+			end
+			return ARTICLES.select {|x| x[:Title].include? search_term or x[:Feed].include? search_term or x[:PublishDate].include? search_term}
 		end
 	end
 end
--- app/javascript/components/articles_list.jsx
+++ app/javascript/components/articles_list.jsx
@@ -3,60 +3,86 @@ import ReactDOM from "react-dom/client";
 
 
 function ArticlesList() {
-	const [loading, setLoading] = useState(true);
-	const [loadedArticles, setLoadedArticles] = useState([]);
+  const [loadedArticles, setLoadedArticles] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-	useEffect(() => {
-		// hit server and get articles
-		const apiEndpoint = "/api/articles"
+  const onSearchTextChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
 
-		fetch(apiEndpoint)
-			.then(response => response.json())
-			.then(data => {
-				console.log(data)
-				setLoadedArticles(data["articles"]);
-				setLoading(false);
-			})
-	},[])
+  useEffect(() => {
+    // hit server and get articles
+    const apiEndpoint = `/api/articles?search_term=${searchTerm}`
 
-	const loadingSection = (
-		<div>loading</div>
-	)
-	const dataSection = (loadedArticles.map((article, index) =>{
-		return (
-			<a href="#" key={index} className="flex justify-center my-5">
-				<div className=" rounded overflow-hidden border w-full lg:w-6/12 md:w-6/12 bg-white mx-3 md:mx-0 lg:mx-0">
-					<div className="w-full flex justify-between p-3">
-					  <div className="flex">
-						<span className="pt-1 ml-2 font-bold text-sm">{article.Title}</span>
-					  </div>
-					  <span className="px-2 hover:bg-gray-300 cursor-pointer rounded"><i className="fas fa-ellipsis-h pt-2 text-lg"></i></span>
-					</div>
-					<img className="w-full bg-cover"></img>
-					<div className="px-3 pb-2">
-					  <div className="pt-1">
-						<div className="mb-2 text-sm">
-						  <span className="font-medium mr-2">Description:</span>{article.Description}
-						</div>
-					  </div>
-					  <div className="mb-2">
-						<div className="mb-2 text-sm">
-						  <span className="font-medium mr-2">PublishDate:</span>{article.PublishDate}
-						</div>
-					  </div>
-					</div>
-				  </div>
-			  </a>
-		)
-	}))
+    fetch(apiEndpoint)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setLoadedArticles(data["articles"]);
+      })
+  },[searchTerm])
 
-	if(loading) {
-		return loadingSection
-	} else {
-		return dataSection
-	}
+  function dataSection(onSearchTextChange) {
+    return (
+    <div>
+      <div className="p-4">
+      <label className="sr-only">Search</label>
+      <div className="relative mt-2">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              ></path>
+          </svg>
+        </div>
+        <input onChange={onSearchTextChange} type="text" id="table-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 pl-10 p-2.5   " placeholder="Search for items"></input>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
+          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full">
+                <thead className="bg-gray-200 border-b">
+                  <tr>
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Title
+                    </th>
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Published Date
+                    </th>
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Feed
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {loadedArticles.map((article, index) =>{ return (
+                  <tr key={index} className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      {article.Title}
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      {article.PublishDate}
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      {article.Feed}
+                    </td>
+                  </tr>
+                  )})}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+  }
+
+  return dataSection(onSearchTextChange)
 }
-
 
 const root = ReactDOM.createRoot(document.getElementById("articles-list-container"));
 root.render(<ArticlesList />)
